cd35b37dd8429729d7dd791447f175eba394eae1
putuokky <okkymahes@gmail.com>
2024-05-29 10:00:35 +0800

    perubahan banayak

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $userOwner = User::create([
+            'name' => 'Okky Maheswara',
+            'email' => 'okkymahes@gmail.com',
+            'password' => bcrypt('123456789'),
+        ]);
+    }
+}
